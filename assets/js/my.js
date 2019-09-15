61aa6c09873d7c60ed246f9bbf0ce6e40c9342c1
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2019-09-15 20:55:38 +0630

    Refresh

---
+++ assets/js/my.js
@@ -0,0 +1,106 @@
+$(document).ready(function() {
+	//========================================
+	// Initial Call Functions
+	//========================================
+	marginFromFaceSVG();
+	myHairAnimate();
+
+	//========================================
+	// Sticky Nav Bar
+	//========================================
+	// ...
+
+	// Menu Bar CLick
+	$('#H-Menu_Bar').click(function() {
+		alert('Comming Soon');
+	});
+
+	//========================================
+	// Scroll Down
+	//========================================
+
+	$('.Scroll-Down').click(function() {
+		alert('Comming Soon');
+	});
+
+	//========================================
+	// When Window Resize
+	//========================================
+
+	window.onresize = function() {
+		// Svg from Background Magin
+		marginFromFaceSVG();
+	};
+
+	//========================================
+	// My Face
+	//========================================
+
+	$('#My-Face').on('mouseenter', function() {
+		myFaceScaleOn();
+	});
+
+	$('#My-Face').on('mouseleave', function() {
+		myFaceScaleDown();
+	});
+
+	function myHairAnimate() {
+		var allPolygons = $('#Hair > g > polygon');
+		$(allPolygons).each(function(k, v) {
+			var getRandom = random(20, 4);
+			$(v).addClass('Hair-Animation--' + getRandom);
+		});
+	}
+
+	function myFaceScaleOn() {
+		var allFacePolygons = $('#Face_SKin > g > polygon');
+		$(allFacePolygons).each(function(k, v) {
+			var getRandom = random(5);
+			console.log(minusOrPlus());
+			$(v).css({
+				transform:
+					'scale(1.0' +
+					random(3) +
+					') translateY(' +
+					minusOrPlus() +
+					getRandom +
+					'px) translateX(' +
+					minusOrPlus() +
+					getRandom +
+					'px)'
+			});
+		});
+	}
+
+	function myFaceScaleDown() {
+		var allFacePolygons = $('#Face_SKin > g > polygon');
+		$(allFacePolygons).each(function(k, v) {
+			$(v).removeAttr('style');
+		});
+	}
+
+	//=======================================
+	// Margin Top From Images
+	//========================================
+
+	function marginFromFaceSVG() {
+		var svgHeight = $('#My-Face').height();
+		var offsetTop = $('#My-Face').offset().top;
+		var navigateHeight = 60 / 1.3;
+		// set Height Div
+		$('#First-Slide-Offset-Top').height(svgHeight + offsetTop - navigateHeight);
+	}
+
+	//=======================================
+	// Helper Functions
+	//=======================================
+
+	function random(max, min = 0) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+	function minusOrPlus() {
+		var arr = [ '-', '+' ];
+		return arr[random(arr.length - 1)];
+	}
+});
