e37a37da965d2b9362e2059900ca0ca6e292f8ef
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2022-05-09 01:36:23 +0630

    Update: Final Latest

--- next/static/QEBJsTcUa5wnfgJ_VdIhp/buildManifest.js
+++ next/static/QEBJsTcUa5wnfgJ_VdIhp/buildManifest.js
@@ -2,7 +2,7 @@
   __rewrites: { beforeFiles: [], afterFiles: [], fallback: [] },
   "/": [
     "static/chunks/43-7ecb5080a162bd18.js",
-    "static/chunks/pages/index-f116645dabd5478e.js",
+    "static/chunks/pages/index-af817151000fe88f.js",
   ],
   "/error": ["static/chunks/pages/error-0a004b8b8498208d.js"],
   sortedPages: ["/", "/app", "/error"],
