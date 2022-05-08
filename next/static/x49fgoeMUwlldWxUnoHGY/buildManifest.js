9d341084a499db6e8fba59939b73dddde7485e8f
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2022-05-09 04:06:27 +0630

    Update: Bro

--- next/static/x49fgoeMUwlldWxUnoHGY/buildManifest.js
+++ next/static/x49fgoeMUwlldWxUnoHGY/buildManifest.js
@@ -1,10 +1,1 @@
-(self.__BUILD_MANIFEST = {
-  __rewrites: { beforeFiles: [], afterFiles: [], fallback: [] },
-  "/": [
-    "static/chunks/43-7ecb5080a162bd18.js",
-    "static/chunks/pages/index-ff9e9b2c3c419ffd.js",
-  ],
-  "/error": ["static/chunks/pages/error-0a004b8b8498208d.js"],
-  sortedPages: ["/", "/app", "/error"],
-}),
-  self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/43-7ecb5080a162bd18.js","static/chunks/pages/index-ff9e9b2c3c419ffd.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
