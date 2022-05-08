7e6ea79f479ce287515c86d82df8217c58132fae
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2022-05-09 00:52:31 +0630

    Update: Next Generation

--- next/static/Q_0jY5thRcK9tWMnbQ3qQ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/43-7ecb5080a162bd18.js","static/chunks/pages/index-82fa8d97015eecaf.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ next/static/Q_0jY5thRcK9tWMnbQ3qQ/buildManifest.js
@@ -0,0 +1,10 @@
+(self.__BUILD_MANIFEST = {
+  __rewrites: { beforeFiles: [], afterFiles: [], fallback: [] },
+  "/": [
+    "static/chunks/43-7ecb5080a162bd18.js",
+    "static/chunks/pages/index-82fa8d97015eecaf.js",
+  ],
+  "/_error": ["static/chunks/pages/error-0a004b8b8498208d.js"],
+  sortedPages: ["/", "/_app", "/_error"],
+}),
+  self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
