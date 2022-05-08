b9576d3382c2a01274b271861d6c906cbe1fa690
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2022-05-09 01:08:26 +0630

    Update: Generator

--- next/static/ZM2uzo2osVijgena8vx9q/buildManifest.js
+++ next/static/ZM2uzo2osVijgena8vx9q/buildManifest.js
@@ -2,9 +2,9 @@
   __rewrites: { beforeFiles: [], afterFiles: [], fallback: [] },
   "/": [
     "static/chunks/43-7ecb5080a162bd18.js",
-    "static/chunks/pages/index-82fa8d97015eecaf.js",
+    "static/chunks/pages/index-f116645dabd5478e.js",
   ],
-  "/_error": ["static/chunks/pages/error-0a004b8b8498208d.js"],
-  sortedPages: ["/", "/_app", "/_error"],
+  "/error": ["static/chunks/pages/error-0a004b8b8498208d.js"],
+  sortedPages: ["/", "/app", "/error"],
 }),
   self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
--- next/static/chunks/pages/app-ed8e224e7905dba8.js
+++ next/static/chunks/pages/app-ed8e224e7905dba8.js
@@ -1,1 +1,69 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{6840:function(n,t,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return e(3847)}])},3847:function(n,t,e){"use strict";e.r(t);var r=e(5893);e(2523),e(6774),e(4213),e(2755);function u(n,t,e){return t in n?Object.defineProperty(n,t,{value:e,enumerable:!0,configurable:!0,writable:!0}):n[t]=e,n}t.default=function(n){var t=n.Component,e=n.pageProps;return(0,r.jsx)(t,function(n){for(var t=1;t<arguments.length;t++){var e=null!=arguments[t]?arguments[t]:{},r=Object.keys(e);"function"===typeof Object.getOwnPropertySymbols&&(r=r.concat(Object.getOwnPropertySymbols(e).filter((function(n){return Object.getOwnPropertyDescriptor(e,n).enumerable})))),r.forEach((function(t){u(n,t,e[t])}))}return n}({},e))}},2523:function(){},2755:function(){},4213:function(){},6774:function(){}},function(n){var t=function(t){return n(n.s=t)};n.O(0,[774,179],(function(){return t(6840),t(880)}));var e=n.O();_N_E=e}]);
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+  [888],
+  {
+    6840: function (n, t, e) {
+      (window.__NEXT_P = window.__NEXT_P || []).push([
+        "/_app",
+        function () {
+          return e(3847);
+        },
+      ]);
+    },
+    3847: function (n, t, e) {
+      "use strict";
+      e.r(t);
+      var r = e(5893);
+      e(2523), e(6774), e(4213), e(2755);
+      function u(n, t, e) {
+        return (
+          t in n
+            ? Object.defineProperty(n, t, {
+                value: e,
+                enumerable: !0,
+                configurable: !0,
+                writable: !0,
+              })
+            : (n[t] = e),
+          n
+        );
+      }
+      t.default = function (n) {
+        var t = n.Component,
+          e = n.pageProps;
+        return (0, r.jsx)(
+          t,
+          (function (n) {
+            for (var t = 1; t < arguments.length; t++) {
+              var e = null != arguments[t] ? arguments[t] : {},
+                r = Object.keys(e);
+              "function" === typeof Object.getOwnPropertySymbols &&
+                (r = r.concat(
+                  Object.getOwnPropertySymbols(e).filter(function (n) {
+                    return Object.getOwnPropertyDescriptor(e, n).enumerable;
+                  })
+                )),
+                r.forEach(function (t) {
+                  u(n, t, e[t]);
+                });
+            }
+            return n;
+          })({}, e)
+        );
+      };
+    },
+    2523: function () {},
+    2755: function () {},
+    4213: function () {},
+    6774: function () {},
+  },
+  function (n) {
+    var t = function (t) {
+      return n((n.s = t));
+    };
+    n.O(0, [774, 179], function () {
+      return t(6840), t(880);
+    });
+    var e = n.O();
+    _N_E = e;
+  },
+]);
