fb08dcad6081ad6beebbeb14c7b33579dcde902a
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2022-05-09 04:01:44 +0630

    update: generator updates

--- next/static/chunks/pages/error-0a004b8b8498208d.js
+++ next/static/chunks/pages/error-0a004b8b8498208d.js
@@ -1,1 +1,21 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{1981:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(67)}])}},function(n){n.O(0,[774,888,179],(function(){return _=1981,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+  [820],
+  {
+    1981: function (n, _, u) {
+      (window.__NEXT_P = window.__NEXT_P || []).push([
+        "/error",
+        function () {
+          return u(67);
+        },
+      ]);
+    },
+  },
+  function (n) {
+    n.O(0, [774, 888, 179], function () {
+      return (_ = 1981), n((n.s = _));
+      var _;
+    });
+    var _ = n.O();
+    _N_E = _;
+  },
+]);
--- next/static/x49fgoeMUwlldWxUnoHGY/buildManifest.js
+++ next/static/x49fgoeMUwlldWxUnoHGY/buildManifest.js
@@ -2,7 +2,7 @@
   __rewrites: { beforeFiles: [], afterFiles: [], fallback: [] },
   "/": [
     "static/chunks/43-7ecb5080a162bd18.js",
-    "static/chunks/pages/index-af817151000fe88f.js",
+    "static/chunks/pages/index-ff9e9b2c3c419ffd.js",
   ],
   "/error": ["static/chunks/pages/error-0a004b8b8498208d.js"],
   sortedPages: ["/", "/app", "/error"],
