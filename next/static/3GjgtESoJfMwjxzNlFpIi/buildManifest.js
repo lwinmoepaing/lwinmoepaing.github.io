6356a811c8a1c105a62cf2c7307a18b6c0a48ac1
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2022-05-11 22:54:19 +0630

    Update: New Svgs And Projects

---
+++ next/static/3GjgtESoJfMwjxzNlFpIi/buildManifest.js
@@ -0,0 +1,10 @@
+(self.__BUILD_MANIFEST = {
+  __rewrites: { beforeFiles: [], afterFiles: [], fallback: [] },
+  "/": [
+    "static/chunks/314-9fb3d071806d24ae.js",
+    "static/chunks/pages/index-d205fed95d817052.js",
+  ],
+  "/error": ["static/chunks/pages/error-0a004b8b8498208d.js"],
+  sortedPages: ["/", "/app", "/error"],
+}),
+  self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
