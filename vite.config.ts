ef60634dce1dde4caebfa663a54a1a3a05bc43e3
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2023-08-02 03:04:01 +0630

    Fixed: Vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base:
-    process.env.NODE_ENV === "production" ? "/lwinmoepaing.github.io/" : "/",
+    process.env.NODE_ENV === "production" ? "https://lwinmoepaing.github.io/" : "/",
   plugins: [
     eslint(),
     {
