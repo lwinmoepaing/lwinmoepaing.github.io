61ce36b57e5d9001084c3f3dd41a9c300920936d
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2023-08-02 03:22:11 +0630

    Adding: OG Facebook Cover Image

--- src/components/Common/MetaHead/MetaHead.tsx
+++ src/components/Common/MetaHead/MetaHead.tsx
@@ -8,13 +8,6 @@ interface MetaHeadProps {
 
 export default function MetaHead(props: MetaHeadProps) {
   const location = useLocation();
-  let url = location.pathname;
-  let origin = "/";
-
-  if (typeof window !== "undefined") {
-    url = window?.location?.origin + location.pathname;
-    origin = window?.location?.origin;
-  }
 
   return (
     <>
@@ -22,11 +15,14 @@ export default function MetaHead(props: MetaHeadProps) {
       <Meta name="twitter:card" content="summary_large_image" />
       <Meta name="twitter:title" content={props.title || ""} />
       <Meta name="twitter:description" content={props.body || ""} />
-      <Meta property="og:url" content={url} />
+      <Meta property="og:url" content={location.pathname} />
       <Meta property="og:type" content="article" />
       <Meta property="og:title" content={props.title || ""} />
       <Meta property="og:description" content={props.body || ""} />
-      <Meta property="og:image" content={`${origin}/images/og_facebook_lmp.jpg`} />
+      <Meta
+        property="og:image"
+        content={`https://lwinmoepaing.github.io/images/og_facebook_lmp.jpg`}
+      />
       <Title>{props.title || ""} | Lwin Moe Paing </Title>
     </>
   );
