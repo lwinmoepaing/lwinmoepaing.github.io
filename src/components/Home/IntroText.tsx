dcfcfa2648cf062b5bd186879415d08efda5c0ba
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2024-02-13 21:02:07 +0630

    add: animation on hero section and butterfly section

---
+++ src/components/Home/IntroText.tsx
@@ -0,0 +1,17 @@
+const IntroText = () => {
+  return (
+    <div class="max-wrapper px-4 sm:px-8 animate-[opacity0to1_0.3s_ease-in-out]">
+      <div class="max-w-[500px] mx-auto my-[3rem] text-center">
+        <h1 class="text-lg my-2">
+          Hello, I'm{" "}
+          <span class="text-indigo-500 text-xl">Lwin Moe Paing !!</span>
+        </h1>
+        <h2 class="text-lg my-2">
+          A <span class="text-indigo-500 font-bold">Frontend Developer</span>{" "}
+          with over 5 years of experience.
+        </h2>
+      </div>
+    </div>
+  );
+};
+export default IntroText;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,12 @@
 import AnimateContainer from "~/components/Common/AnimateContainer/AnimateContainer";
 import MetaHead from "~/components/Common/MetaHead/MetaHead";
-// import SittingAnimation from "~/components/Animation/SittingAnimation/SittingAnimation";
 import HeroSectionAnimation from "~/components/Animation/HeroSectionAnimation/HeroSectionAnimation";
 import ButterflyAnimation from "~/components/Animation/ButterflyAnimation/ButterflyAnimation";
 import SharingTextAnimation from "~/components/Animation/SharingTextAnimation/SharingTextAnimation";
 import CircleBackgroundAnimation from "~/components/Animation/CircleBackgroundAnimation/CircleBackgroundAnimation";
 import WaveTextAnimation from "~/components/Animation/WaveTextAnimation/WaveTextAnimation";
 import WalkingWorldAnimation from "~/components/Animation/WalkingWorldAnimation/WalkingWorldAnimation";
+import IntroText from "~/components/Home/IntroText";
 
 function HomePage() {
   return (
@@ -23,24 +23,10 @@ function HomePage() {
             </div>
           </div>
 
-          <div class="max-wrapper px-4 sm:px-8 animate-[opacity0to1_0.3s_ease-in-out]">
-            <div class="max-w-[500px] mx-auto my-[3rem] text-center">
-              <h1 class="text-lg my-2">
-                Hello, I'm{" "}
-                <span class="text-indigo-500 text-xl">Lwin Moe Paing !!</span>
-              </h1>
-              <h2 class="text-lg my-2">
-                A{" "}
-                <span class="text-indigo-500 font-bold">
-                  Frontend Developer
-                </span>{" "}
-                with over 5 years of experience.
-              </h2>
-            </div>
+          <IntroText />
 
-            <div class="max-w-[960px] mx-auto ">
-              <ButterflyAnimation />
-            </div>
+          <div class="max-w-[960px] mx-auto ">
+            <ButterflyAnimation />
           </div>
 
           <div class="mx-auto">
@@ -61,11 +47,6 @@ function HomePage() {
             <WalkingWorldAnimation />
           </div>
 
-          {/* <div class="max-wrapper px-4 sm:px-8 animate-[opacity0to1_0.3s_ease-in-out]">
-            <div class="max-w-[580px] mx-auto">
-              <SittingAnimation />
-            </div>
-          </div> */}
         </div>
       </AnimateContainer>
     </>
